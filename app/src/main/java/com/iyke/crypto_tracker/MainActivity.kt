3aebb266c68f8d454938413b9927530f882db419
emmanueldav <kuradavis987@gmail.com>
2022-06-19 23:41:13 +0100

    important update

--- app/src/main/java/com/iyke/crypto_tracker/MainActivity.kt
+++ app/src/main/java/com/iyke/crypto_tracker/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
+
                     color = MaterialTheme.colors.background
                 ) {
                     Greeting("Android")
