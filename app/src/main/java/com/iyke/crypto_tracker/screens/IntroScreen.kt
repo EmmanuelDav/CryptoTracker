55e47d846603e20dd4c9e17a5fe68936ef131bb6
emmanueldav <kuradavis987@gmail.com>
2022-06-22 12:04:18 +0100

    added important feature

--- app/src/main/java/com/iyke/crypto_tracker/screens/IntroScreen.kt
+++ app/src/main/java/com/iyke/crypto_tracker/screens/IntroScreen.kt
@@ -6,12 +6,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -38,7 +40,8 @@ class IntroScreen : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CryptoTrackerTheme {
-                Surface(modifier = Modifier.fillMaxSize(),
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
                     Greeting(name = "")
@@ -69,56 +72,66 @@ fun Greeting(name: String) {
 @Composable
 fun Screen() {
     val context = LocalContext.current
+    Column {
+        Card(elevation = 4.dp) {
 
-    Card(elevation = 4.dp) {
-        Image(painter = painterResource(id = R.drawable.action), contentDescription = null, modifier = Modifier.fillMaxWidth(),
-        contentScale = ContentScale.FillHeight)
-        Column(
-            verticalArrangement = Arrangement.Bottom,
-            horizontalAlignment = Alignment.Start,
-            modifier = Modifier
-                .padding(22.dp)
-                .fillMaxHeight()
-        ) {
-            Text(
-                text = "The World Fastest Crypto Growing App",
-                style = MaterialTheme.typography.body1,
-                fontSize = 34.sp,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center
+            Image(
+                painter = painterResource(id = R.drawable.action),
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.FillHeight
             )
-            Spacer(modifier = Modifier.height(10.dp))
-            Text(
-                text = "Grow your portfolio by receiving rewards on your crypto growing asset!",
-                fontSize = 18.sp,
-                style = MaterialTheme.typography.body2,
-                color = LightGrayColor,
-                textAlign = TextAlign.Center
+            Box(modifier = Modifier.fillMaxSize()){
+                Column(
+                    modifier = Modifier.background(color = Color.Green, shape = RoundedCornerShape(5)).align(Alignment.BottomCenter),
 
-            )
-            Spacer(modifier = Modifier.height(45.dp))
-            Box(Modifier.fillMaxWidth(), contentAlignment = Alignment.Center){
-                Button(
-                    modifier = Modifier
-                        .background(
-                            Brush.horizontalGradient(
-                                colors = listOf(
-                                    PinkColor, blue
+
+                    ) {
+                    Spacer(modifier = Modifier.height(35.dp))
+
+                    Text(
+                        text = "The World Fastest Crypto Growing App",
+                        style = MaterialTheme.typography.body1,
+                        fontSize = 34.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center
+                    )
+                    Spacer(modifier = Modifier.height(10.dp))
+                    Text(
+                        text = "Grow your portfolio by receiving rewards on your crypto growing asset!",
+                        fontSize = 18.sp,
+                        style = MaterialTheme.typography.body2,
+                        color = LightGrayColor,
+                        textAlign = TextAlign.Center
+
+                    )
+                    Spacer(modifier = Modifier.height(45.dp))
+                    Box(Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                        Button(
+                            modifier = Modifier
+                                .background(
+                                    Brush.horizontalGradient(
+                                        colors = listOf(
+                                            PinkColor, blue
+                                        )
+                                    ),
+                                    RoundedCornerShape(20.dp)
                                 )
+                                .height(58.dp),
+                            colors = ButtonDefaults.buttonColors(
+                                backgroundColor = Color.Transparent
                             ),
-                            RoundedCornerShape(20.dp)
-                        )
-                        .height(58.dp),
-                    colors = ButtonDefaults.buttonColors(
-                        backgroundColor = Color.Transparent
-                    ),
-                    onClick = {
-                           context.startActivity(Intent(context,MainActivity::class.java))
-                    },
-                    elevation = ButtonDefaults.elevation(0.dp, 0.dp)
-                ) {
-                    Text(text = "Get Started", fontSize = 20.sp, color = Color.White)
+                            onClick = {
+                                context.startActivity(Intent(context, MainActivity::class.java))
+                            },
+                            elevation = ButtonDefaults.elevation(0.dp, 0.dp)
+                        ) {
+                            Text(text = "Get Started", fontSize = 20.sp, color = Color.White)
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(35.dp))
                 }
+
             }
         }
     }
